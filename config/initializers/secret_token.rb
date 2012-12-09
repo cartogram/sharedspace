52dd32d16e433388e7e337fca57e3d3387d30242
Matthew Seccafien <matt@studiocartogram.com>
2012-12-09 13:26:02 -0500

    created rails application and statuses

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharedspace::Application.config.secret_token = 'e0aaef9b844b809b7f4fcfe7f3b97067b38fb0ad8225c60ffca5f355342c5ee4962b074a660ef361368d7081abc9d266a4198468b6892ae9f34a904518edbeea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Sharedspace::Application.config.session_store :cookie_store, key: '_sharedspace_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Sharedspace::Application.config.session_store :active_record_store
